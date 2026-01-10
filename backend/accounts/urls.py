ac5e2cd1c515a75226111bd587f5be153768bcf3
Gabriel Lima <49318475+yuusha-sama@users.noreply.github.com>
2026-01-10 18:04:41 -0300

    mais uma vez... corrigindo erro do... URLs....

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.healthcheck, name="healthcheck"),
     path("register/", views.register, name="register"),
-    path("login/", views.login, name="login"),
+    path("login/", views.login_view, name="login"),
+    path("me/", views.me, name="me"),
+    path("logout/", views.logout_view, name="logout"),
 ]
