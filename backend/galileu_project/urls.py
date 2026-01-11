c890d83b0ae7a401943ead42a1798a6f0558af83
Gabriel Lima <49318475+yuusha-sama@users.noreply.github.com>
2026-01-10 23:28:16 -0300

    correção de bugs

--- backend/galileu_project/urls.py
+++ backend/galileu_project/urls.py
@@ -3,5 +3,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    # funciona direto no Django:
     path("auth/", include("accounts.urls")),
+
+    # funciona quando o front chama /api/auth/...:
+    path("api/auth/", include("accounts.urls")),
 ]
